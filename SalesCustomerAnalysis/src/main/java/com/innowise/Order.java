7c1460e7e4cbecd94a6bfed2645877940e0199b7
Tonny327 <pikulik96@mail.ru>
2025-09-04 23:25:32 +0300

    feat: add main class for orders analytics

--- SalesCustomerAnalysis/src/main/java/com/innowise/Order.java
+++ SalesCustomerAnalysis/src/main/java/com/innowise/Order.java
@@ -5,6 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -13,5 +14,7 @@ public class Order {
     private String orderId;
     private LocalDateTime orderDate;
     private Customer customer;
+    private List<OrderItem> items;
+    private OrderStatus status;
 
 }
---
+++ SalesCustomerAnalysis/src/main/java/com/innowise/OrdersAnalyticsService.java
@@ -0,0 +1,12 @@
+package com.innowise;
+
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class OrdersAnalyticsService {
+    private final List<Order> orders;
+
+}
