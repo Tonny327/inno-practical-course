5760f7e2a83e2022f0173bf85634243a4d00c0e2
Tonny327 <pikulik96@mail.ru>
2025-09-04 23:30:27 +0300

    feat: add method to get list of unique cities from orders

--- SalesCustomerAnalysis/src/main/java/com/innowise/OrdersAnalyticsService.java
+++ SalesCustomerAnalysis/src/main/java/com/innowise/OrdersAnalyticsService.java
@@ -3,10 +3,24 @@ package com.innowise;
 
 import lombok.AllArgsConstructor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class OrdersAnalyticsService {
     private final List<Order> orders;
+
+    public List<String> getUniqueCities(List<Order> orders) {
+        return orders.stream()
+                .map(order -> order.getCustomer().getCity())
+                .filter(Objects::nonNull)
+                .distinct().collect(Collectors.toList());
+    }
+
 
 }
